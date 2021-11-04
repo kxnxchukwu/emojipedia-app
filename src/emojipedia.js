94b3197290d36796508ee2b9cf761d14bf5d5372
Kenechukwu <kenechukwu.ajufo@gmail.com>
2021-11-04 13:19:50 +0000

    First Commit

---
+++ src/emojipedia.js
@@ -0,0 +1,88 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "😀",
+    name: "Grinning Face",
+    meaning:
+      "A yellow face with simple, open eyes and a broad, open smile, showing upper teeth and tongue on some platforms. Often conveys general pleasure and good cheer or humor."
+  },
+  {
+    id: 5,
+    emoji: "😉",
+    name: "Winking Face",
+    meaning:
+      "A yellow face with a slight smile shown winking, usually its left eye. May signal a joke, flirtation, hidden meaning, or general positivity. Tone varies, including playful, affectionate, suggestive, or ironic."
+  },
+  {
+    id: 6,
+    emoji: "🥰",
+    name: "Smiling Face with Hearts",
+    meaning:
+      "A yellow face with smiling eyes, a closed smile, rosy cheeks, and several hearts floating around its head. Expresses a range of happy, affectionate feelings, especially being in love. The number of hearts (three or more) varies by platform."
+  },
+  {
+    id: 7,
+    emoji: "🤑",
+    name: "Money-Mouth Face",
+    meaning:
+      "A yellow face with raised eyebrows, dollar signs for eyes, and an open smile sticking out a tongue styled after a green, dollar banknote. Some platforms depict the dollar-sign eyes in green circles."
+  },
+  {
+    id: 8,
+    emoji: "🤐",
+    name: "Zipper-Mouth Face",
+    meaning:
+      "A yellow face with simple, open eyes and a closed zipper for a mouth. Meaning widely varies, but commonly conveys a secret or that someone will keep one (e.g., My lips are sealed). May also be used to tell someone to stop talking (e.g., Zip it! or I’ll shut up now)."
+  },
+  {
+    id: 9,
+    emoji: "🥵",
+    name: "Hot Face",
+    meaning:
+      "A reddish-orange face with furrowed eyebrows, tongue stuck out, and beads of sweat, as if overheated from high temperatures. Number and position of sweat droplets vary by platforms. May also represent physical attractiveness (slang, hot)."
+  },
+  {
+    id: 10,
+    emoji: "🥶",
+    name: "Cold Face",
+    meaning:
+      "An icy-blue face with gritted teeth usually depicted with icicles clinging to its cheeks or jaw, as if frozen from extreme cold. May also represent unfriendliness (slang, cold) or excellence (slang, cool or chill)."
+  },
+  {
+    id: 11,
+    emoji: "😫",
+    name: "Tired Face",
+    meaning:
+      "A yellow face with scrunched, X-shaped eyes, furrowed eyebrows, and a broad, open frown, as if yawning in exhaustion or groaning in exasperation. Usually shown with upper teeth and sometimes tongue. While intended to represent tiredness, it commonly conveys various degrees and tones of frustration and sadness as well excitement and affection, as if it just can’t handle how great someone or something is."
+  },
+  {
+    id: 12,
+    emoji: "🥱",
+    name: "Yawning Face",
+    meaning:
+      "A yellow face with eyes closed and mouth wide open covered by a hand. Captured mid-yawn, this may represent having insufficient sleep, or to imply boredom with a person or topic."
+  }
+];
+
+export default emojipedia;
